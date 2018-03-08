a5ff58f90110384bdb6069bc1de6cdcb44e3e7e5
TN-10 <nelio.codices@gmail.com>
2018-03-08 13:08:48 +0000

    changes have been added

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -7,9 +7,47 @@ using System.Threading.Tasks;
 namespace MyFirstMethod
 {
     class Program
+
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// count to 10
+        /// </summary>
+        static void CountTo10()
         {
+            for (int i = 1; i <= 10; i++)
+                Console.WriteLine(i);
+
         }
+        /// <summary>
+        /// count to n
+        /// </summary>
+        /// <param name="n"></param>
+        static void CountToN(int n)
+        {
+            for (int i = 1; i <= n; i++)
+                Console.WriteLine(i);
+
+
+        }
+
+        /// <summary>
+        /// This is my main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+
+        {   
+         
+            CountTo10();
+            CountTo10();
+            CountToN(3);
+            CountToN(4);
+
+
+        }
+
     }
+
 }
+
+
